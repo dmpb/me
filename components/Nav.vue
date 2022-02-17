83988839564032f01e58f192af752f0c568567f8
Daniel Ponce <dmep17@gmail.com>
2022-02-16 23:54:42 -0500

    Añadir foto de perfil

---
+++ components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="px-3 py-8 bg-transparent">
+    <div>
+      <h1 class="text-base text-white">
+        Daniel
+        <span class="font-bold">Ponce</span>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <Tutorial/>
+  <div class="relative h-screen overflow-hidden bg-black">
+    <!-- Container -->
+    <div class="container h-full px-3 mx-auto">
+      <div class="grid h-full grid-cols-8">
+        <div class="col-span-3 pt-24">
+          <p class="font-bold text-white text-7xl">
+            Hola, mi nombre es
+            <span class="text-red-500">Daniel Ponce</span>
+          </p>
+        </div>
+        <div class="flex items-end col-span-5">
+          <img
+            src="~/assets/images/daniel-ponce-profile.jpg"
+            width="100%"
+            alt="Perfil de Daniel Ponce"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'Index',
 }
 </script>
