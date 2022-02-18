9649ae9cb4fbb297f8622813d08f806bc94b3498
Daniel Ponce <dmep17@gmail.com>
2022-02-17 22:45:34 -0500

    Corregir error de alcance de propiedades

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,14 +18,12 @@ export default {
     htmlAttrs: {
       lang: "es",
     },
+    link: [{ rel: "icon", type: "image/x-icon", href: url + "/favicon.ico" }],
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: url + "/favicon.ico" }],
-    meta: [
       {
         name: "description",
         content: "Hola, me llamo Daniel Ponce",
